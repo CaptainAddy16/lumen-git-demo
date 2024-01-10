3a3e1efaf941612ae5e5422a8a4b0a90393d5338
Aditya Patil <Aditya.Patil@lumen.com>
2024-01-10 14:15:25 +0530

    Content changed again

--- src/main/java/com/example/demo/apis/sampleApi.java
+++ src/main/java/com/example/demo/apis/sampleApi.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class sampleApi {
 @GetMapping
     public ResponseEntity<String> get(){
-    return ResponseEntity.ok("hello there");
+    return ResponseEntity.ok("hello master");
 }
 }
